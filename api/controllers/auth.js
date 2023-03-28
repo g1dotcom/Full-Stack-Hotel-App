a3a6afe3a1ca0b08db25162e470ff5373f986aea
Gökhan Süle <thegkhnsl@gmail.com>
2023-03-28 19:57:32 +0300

    jwt kuruldu bcrypt kuruldu ve login işlemleri

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,15 +1,45 @@
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import { createError } from "../utils/error.js";
 
+// REGISTER
 export const register = async (req, res, next) => {
   try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hash,
     });
     await newUser.save();
     res.status(200).send("User created successfully");
   } catch (error) {
     next(err);
+  }
+};
+//LOGIN
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return res.status(400).json({ message: "User does not exist" });
+
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Incorrect password" });
+
+    const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, "secret", {
+      expiresIn: "1d",
+    });
+
+    const { password, isAdmin, ...otherDetails } = user._doc;
+
+    res.status(200).json({ ...otherDetails });
+  } catch (error) {
+    res.status(500).json(error);
   }
 };

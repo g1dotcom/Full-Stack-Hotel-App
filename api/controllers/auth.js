b0d75e4ce76ebb070dc62680682fac8830c0a475
Gökhan Süle <thegkhnsl@gmail.com>
2023-03-28 17:55:01 +0300

    register işlemi tamamlandı

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,14 @@
+import User from "../models/User.js";
+
 export const register = async (req, res, next) => {
   try {
-    const newUser = new User(req.body);
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    await newUser.save();
+    res.status(200).send("User created successfully");
   } catch (error) {
     next(err);
   }

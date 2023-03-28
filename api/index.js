687fa427e81087122ae6d3d1dd679c2a690ff0ec
Gökhan Süle <thegkhnsl@gmail.com>
2023-03-28 15:02:54 +0300

    mongoDb ile bağlantı kuruldu

--- api/index.js
+++ api/index.js
@@ -1,6 +1,24 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
+
+dotenv.config();
+
+// Connect to DB
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Disconnected from mongoDB");
+});
 
 app.listen(8800, () => {
   console.log("Server is running on port 8800");

aec4a820bc3ebb9c980d9ea05957972e1a4e9f17
Gökhan Süle <thegkhnsl@gmail.com>
2023-03-29 17:11:05 +0300

    Router created for rooms

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,5 +1,27 @@
 import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+  updateRoom,
+} from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+//CREATE ROOM
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+//UPDATE ROOM
+router.put("/:id", verifyAdmin, updateRoom);
+
+//DELETE ROOM
+router.delete("/:id", verifyAdmin, deleteRoom);
+
+//GET ROOM
+router.get("/:id", getRoom);
+
+//GET ALL ROOMS
+router.get("/", getRooms);
 export default router;

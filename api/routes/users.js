e4bcd653ad2e0ad859fcfd089aadb33dc5dde0fa
Gökhan Süle <thegkhnsl@gmail.com>
2023-03-29 14:11:31 +0300

    userlar için router oluşturuldu.

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,23 @@
 import express from "express";
+import {
+  deleteUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+} from "../controllers/user.js";
 
 const router = express.Router();
+
+//UPDATE
+router.put("/:id", updateUser);
+
+//DELETE
+router.delete("/:id", deleteUser);
+
+//GET USER
+router.get("/:id", getUser);
+
+//GET ALL USERS
+router.get("/", getAllUsers);
 
 export default router;

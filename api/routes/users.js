19005b8f43580546626336057442c76de76d91bd
Gökhan Süle <thegkhnsl@gmail.com>
2023-03-29 14:51:09 +0300

    verifyToken oluşturuldu

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,8 +5,13 @@ import {
   getUser,
   updateUser,
 } from "../controllers/user.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+  res.status(200).send({ message: "Hello User , You are authenticated!" });
+});
 
 //UPDATE
 router.put("/:id", updateUser);

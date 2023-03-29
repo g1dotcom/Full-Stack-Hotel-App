b0c1547d181c105defb5ca34bf2cdfcfe9e13771
Gökhan Süle <thegkhnsl@gmail.com>
2023-03-29 16:52:36 +0300

    create room controllers yazıldı

---
+++ api/controllers/room.js
@@ -0,0 +1,21 @@
+import Room from "../models/Room.js";
+import Hotel from "../models/Hotel.js";
+
+export const createRoom = async (req, res, next) => {
+  const hotelId = req.params.hotelId;
+  const newRoom = new Room(req.body);
+
+  try {
+    const savedRoom = await newRoom.save();
+    try {
+      await Hotel.findByIdAndUpdate(hotelId, {
+        $push: { rooms: savedRoom._id },
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+    res.status(200).json(savedRoom);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};

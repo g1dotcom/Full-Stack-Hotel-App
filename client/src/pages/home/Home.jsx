428c8038ee7e67bfecce874b54e08de16267de4c
Gökhan Süle <thegkhnsl@gmail.com>
2023-03-30 18:59:24 +0300

    property.jsx finished and imported home.jsx

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import "./home.css";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 import Featured from "../../components/featured/Featured";
+import PropertyList from "../../components/propertyList/PropertyList";
 
 const Home = () => {
   return (
@@ -14,6 +15,8 @@ const Home = () => {
       <div className="homeContainer">
         <Featured />
         <h1 className="homeTitle">Browse by property type </h1>
+        <PropertyList />
+        <h1 className="homeTitle">Home guestest love</h1>
       </div>
     </div>
   );

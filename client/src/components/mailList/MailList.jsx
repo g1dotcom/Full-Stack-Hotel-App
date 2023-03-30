720a2e1ffccb6b9c99267110c7e99d4afc43110c
Gökhan Süle <thegkhnsl@gmail.com>
2023-03-30 19:53:55 +0300

    MailList.jsx finished and imported to Home.jsx

--- client/src/components/mailList/MailList.jsx
+++ client/src/components/mailList/MailList.jsx
@@ -1,7 +1,18 @@
 import "./mailList.css";
 
 const MailList = () => {
-  return <div>MailList</div>;
+  return (
+    <div className="mail">
+      <h1 className="mailTitle">Save Time, Save Money</h1>
+      <span className="mailDesc">
+        Sign up and we will send the best deals to you
+      </span>
+      <div className="mailInputContainer">
+        <input type="text" placeholder="Your email" className="mailInput" />
+        <button className="mailButton">Subscribe</button>
+      </div>
+    </div>
+  );
 };
 
 export default MailList;

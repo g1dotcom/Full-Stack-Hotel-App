9c657b0d64e148742b5b4cccde72e3bd9dd159e2
Gökhan Süle <thegkhnsl@gmail.com>
2023-03-30 14:45:45 +0300

    navbar is finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,21 @@
+//REACT ROUTER DOM
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//PAGES
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
+
 function App() {
-  return <div className="App">sa</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/hotels" element={<List />}></Route>
+        <Route path="/hotels/:id" element={<Hotel />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+//PAGES
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));

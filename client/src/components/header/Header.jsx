65b340c706d5c3d0c53a7e5288bd9ebec5a19b3a
Gökhan Süle <thegkhnsl@gmail.com>
2023-03-30 15:22:01 +0300

    i date-range and header edit

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -4,7 +4,9 @@ import "./header.css";
 //fontawesome
 import {
   faBed,
+  faCalendarDays,
   faCar,
+  faPerson,
   faPlane,
   faTaxi,
 } from "@fortawesome/free-solid-svg-icons";
@@ -44,6 +46,24 @@ const Header = () => {
         <div className="headerSearch">
           <div className="headerSearchItem">
             <FontAwesomeIcon icon={faBed} className="headerIcon" />
+            <input
+              type="text"
+              placeholder="Where are you going?"
+              className="headerSearchInput"
+            />
+          </div>
+
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+            <span className="headerSearchText">date to date</span>
+          </div>
+
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+            <span className="headerSearchText">2 adults 2 children 1 room</span>
+          </div>
+          <div className="headerSearchItem">
+            <button className="headerBtn">Search</button>
           </div>
         </div>
       </div>

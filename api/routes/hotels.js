a2b039c6d892d8f89da673ee67f3d21d62e6f25e
Gökhan Süle <thegkhnsl@gmail.com>
2023-04-01 14:38:01 +0300

    getCountByCities controllers and routes created

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -6,6 +6,7 @@ import {
   createHotel,
   getHotel,
   getHotels,
+  countByCity,
 } from "../controllers/hotel.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
@@ -18,8 +19,10 @@ router.put("/:id", verifyAdmin, updateHotel);
 //DELETE HOTEL
 router.delete("/:id", verifyAdmin, deleteHotel);
 //GET HOTEL
-router.get("/:id", getHotel);
+router.get("/find/:id", getHotel);
 //GET ALL HOTELS
 router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("countByType", getHotels);
 
 export default router;

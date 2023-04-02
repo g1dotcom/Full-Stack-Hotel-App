fc88b941fb55ec43d0c4804834814d0a6c814e2a
Gökhan Süle <thegkhnsl@gmail.com>
2023-04-02 14:29:22 +0300

    login.jsx and login.css finished

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -3,35 +3,33 @@ import bcrypt from "bcryptjs";
 import { createError } from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
-// REGISTER
 export const register = async (req, res, next) => {
   try {
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.password, salt);
 
     const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
+      ...req.body,
       password: hash,
     });
+
     await newUser.save();
-    res.status(200).send("User created successfully");
-  } catch (error) {
+    res.status(200).send("User has been created.");
+  } catch (err) {
     next(err);
   }
 };
-//LOGIN
 export const login = async (req, res, next) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    if (!user) return res.status(400).json({ message: "User does not exist" });
+    if (!user) return next(createError(404, "User not found!"));
 
     const isPasswordCorrect = await bcrypt.compare(
       req.body.password,
       user.password
     );
     if (!isPasswordCorrect)
-      return res.status(400).json({ message: "Incorrect password" });
+      return next(createError(400, "Wrong password or username!"));
 
     const token = jwt.sign(
       { id: user._id, isAdmin: user.isAdmin },
@@ -39,14 +37,13 @@ export const login = async (req, res, next) => {
     );
 
     const { password, isAdmin, ...otherDetails } = user._doc;
-
     res
       .cookie("access_token", token, {
         httpOnly: true,
       })
       .status(200)
-      .json({ ...otherDetails });
-  } catch (error) {
-    res.status(500).json(error);
+      .json({ details: { ...otherDetails }, isAdmin });
+  } catch (err) {
+    next(err);
   }
 };

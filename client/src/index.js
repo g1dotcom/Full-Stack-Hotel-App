b7d5733bb2b465c0256b361510485cea9499db6a
Gökhan Süle <thegkhnsl@gmail.com>
2023-04-02 13:30:58 +0300

    created context

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 
 //PAGES
 import App from "./App";
+//CONTEXT
+import { SearchContextProvider } from "./context/SearchContex";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <SearchContextProvider>
+      <App />
+    </SearchContextProvider>
   </React.StrictMode>
 );

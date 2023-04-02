60bda9d75608de4d36ac1d4286053f8dadfc42f0
Gökhan Süle <thegkhnsl@gmail.com>
2023-04-02 14:02:25 +0300

    AuthContext Created

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 //CONTEXT
 import { SearchContextProvider } from "./context/SearchContex";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <SearchContextProvider>
-      <App />
-    </SearchContextProvider>
+    <AuthContextProvider>
+      <SearchContextProvider>
+        <App />
+      </SearchContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
